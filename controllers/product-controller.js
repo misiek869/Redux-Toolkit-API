af9fa5b619715519a47c3fd7a17d367829cfa7ab
Michal Gawik <gawlik.zator@o2.pl>
2024-10-24 11:51:18 +0200

    create controller

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,8 @@
 const Product = require('../models/product')
 
-const getAllProducts = async (req, res) => {}
+const getAllProducts = async (req, res) => {
+	res.status(200).json({ products, nbHits: products.length })
+}
 
 module.exports = {
 	getAllProducts,
