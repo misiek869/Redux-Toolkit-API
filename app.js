afd350f4b4765da210bfda75058008a93bed42b8
Michal Gawik <gawlik.zator@o2.pl>
2024-10-24 13:27:42 +0200

    add cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const connectDB = require('./db/connect')
 const productsRouter = require('./routes/products-route')
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
